f4163d352583f46174523df8caee5d6266eac7fb
Danielle R Cherry <daniellecherry@pursuit.org>
2020-02-01 17:29:42 -0500

    User create post function in query

--- backend/queries/posts.js
+++ backend/queries/posts.js
@@ -1,16 +1,29 @@
 const db = require("../db/indexs");
 
+const createPost = async (req, res, next) => {
+  try {
+    let post = await db.one("INSERT INTO posts (user_post_id, body, created) VALUES(${user_posts_id}), ${body}, ${created}) RETURNING *", req.body);
+    res.status(200).json({
+      post,
+      status: "success",
+      message: "user created post"
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
 const getPostsForUser = async (req, res, next) => {
   try {
     let posts = await db.any("SELECT * FROM posts WHERE user_posts_id = $1", req.params.id);
     res.status(200).json({
       posts,
       status: "success",
-      message: "user post retrieved"
+      message: "user posts retrieved"
     })
   } catch(err) {
     next(err);
   }
 }
 
-module.exports = {getPostsForUser}
+module.exports = {getPostsForUser, createPost}
