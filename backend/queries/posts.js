d74f256ffc52cd6fdca8f74a7648f74604fdef99
Danielle R Cherry <daniellecherry@pursuit.org>
2020-02-01 18:05:05 -0500

    Change main key in package.json

--- backend/queries/posts.js
+++ backend/queries/posts.js
@@ -1,8 +1,8 @@
-const db = require("../db/indexs");
+const db = require("../db/index");
 
 const createPost = async (req, res, next) => {
   try {
-    let post = await db.one("INSERT INTO posts (user_post_id, body, created) VALUES(${user_posts_id}), ${body}, ${created}) RETURNING *", req.body);
+    let post = await db.one("INSERT INTO posts (user_post_id, body) VALUES(${user_posts_id}), ${body}) RETURNING *", req.body);
     res.status(200).json({
       post,
       status: "success",
