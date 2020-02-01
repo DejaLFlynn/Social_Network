43b09d8c134ced786c5782182011de757aa37831
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-01 18:00:05 -0500

    comments queries

---
+++ backend/queries/comments.js
@@ -0,0 +1,54 @@
+const db = require("../db/index")
+
+const addComment = async(req,res,next)=>{
+    try{
+        await db.one("INSERT INTO comments (user_comments_id, post_comment_id, body)VALUES ${user_comments_id}${post_comment_id}${body}", req.body)
+        response.status(200).json({
+            status: "success",
+            message: "NEW COMMENT" 
+        })
+    }catch(err){
+        next(err)
+    }
+}
+const deleteComment = async(req,res,next)=>{
+    try{
+        await db.none("DELETE FROM comments WHERE id = $1 ", request.params.id)
+        response.status(200).json({
+            status: "success",
+            message: "COMMENT DELETED" 
+        })
+    }catch(err){
+        next(err)
+    }
+}
+const getAllComments =async(request,response,next) =>{
+    try{
+
+        let comments=await db.any("SELECT * FROM comments")
+        response.status(200).json({
+            comments, 
+            status: "success",
+            message: "ALL COMMENTS"
+        })
+    }catch(err){
+        next(err)
+    }
+}
+const getComment = async(request, response,next)=>{
+    try{
+        let comment = await db.one(
+            "SELECT * FROM comments WHERE id = $1 ", [request.params.id]
+
+        )
+        response.status(200).json({
+            comment, 
+            status: "success",
+            message: "ONE COMMENT FETCHED"
+        })
+
+    }catch(err){
+        next(err)
+    }
+}
+module.exports = {getAllComments,getComment,addComment,deleteComment}
