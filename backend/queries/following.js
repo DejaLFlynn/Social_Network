97e8de206c5713ab459b25548f4b3c1e18c30aa3
Nilber Remon <nilberremon@gmail.com>
2020-02-03 18:49:20 -0500

    Following Haters Queries Build-Out

--- backend/queries/following.js
+++ backend/queries/following.js
@@ -1,5 +1,27 @@
 const db = require("../db/index")
 
+const addHater = async (req,res,next) => {
+    try {
+        let hater = await db.none("INSERT into haters (curr_user, hater_id) VALUES (${curr_user}, ${hater_id}", req.body)
+        res.status(200).json({
+            status: "Success",
+            message: "Following Hater"
+        })
+    }
+}
+
+
+const removeHater = async (req,res,next) => {
+    try {
+        let hater = await db.none("DELETE from haters WHERE id = $1", req.params.id)
+        res.status(200).json({
+            status: "Success",
+            message: "Unfollowed Hater"
+        })
+    }
+}
+
+
 const getHaters = async (req,res,next) => {
     try {
         let haters = await db.any("SELECT * from haters WHERE curr_user = $1", req.params.id)
@@ -12,5 +34,18 @@ const getHaters = async (req,res,next) => {
         next(error)
     }
 }
+
+const getHaterFeed = async (req,res,next) => {
+    try {
+        let haterFeed = await db.any("SELECT * from haters INNER JOIN posts ON hater_id = user_post_id WHERE curr_user = $1", req.params.id)
+        res.status(200).json({
+            haterFeed,
+            status: "Success",
+            message: "Hater Feed Extracted"
+        })
+    }
+}
+
+
 
 module.exports = {getHaters}
