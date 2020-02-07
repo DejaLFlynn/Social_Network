dfd4dc50f6ba68b32aabc97edc2900a5ef3eeadb
Nilber Remon <nilberremon@gmail.com>
2020-02-07 15:40:10 -0500

    Designing Style and Display Add Post Pop-Up Form

--- frontend/profilePage/index.js
+++ frontend/profilePage/index.js
@@ -1,60 +1,47 @@
-document.addEventListener("DOMContentLoaded", ()=> {
 
-  const getUserHeader = async () => {
-    let img = document.querySelector("#profilePic")
-    let username = document.querySelector("#username")
-    try {
-      // let res = await axios.get(`http://localhost:3000/users/${sessionStorage.id}`)
-      // username.innerText = res.data.users.username
-      // img.src = res.data.users.photo_url
-      let res = await axios.get(`http://localhost:3000/users/`)
-      username.innerText = res.data.users[0].username
-      img.src = res.data.users[0].photo_url
-    } catch (error) {
-      console.log(error)
-    }
+
+const getUserHeader = async () => {
+  let img = document.querySelector("#profilePic")
+  let username = document.querySelector("#username")
+  try {
+    // let res = await axios.get(`http://localhost:3000/users/${sessionStorage.id}`)
+    // username.innerText = res.data.users.username
+    // img.src = res.data.users.photo_url
+    let res = await axios.get(`http://localhost:3000/users/`)
+    username.innerText = res.data.users[0].username
+    img.src = res.data.users[0].photo_url
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getUserPosts = async () => {
+  let img = document.querySelector("#postPic")
+  let caption = document.querySelector("#caption")
+  try {
+    let res = await axios.get("http://localhost:3000/posts")
+    img.src = res.data.posts[0].photo_url
+      caption.innerText = res.data.posts[0].body
+  } catch (error) {
+    console.log(error)
+  }
+}
+getUserHeader();
+getUserPosts()
+
+
+
+
+const openForm = () => {
+    document.querySelector(".addPostFormContainer").style.display = "block";
+  }
+const closeForm = () =>{
+    document.querySelector(".addPostFormContainer").style.display = "none";
   }
 
-  const getUserPosts = async () => {
-    let img = document.querySelector("#postPic")
-    let caption = document.querySelector("#caption")
-    try {
-      let res = await axios.get("http://localhost:3000/posts")
-      img.src = res.data.posts[0].photo_url
-       caption.innerText = res.data.posts[0].body
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  getUserHeader();
-  getUserPosts()
-})
 
-// let form = document.querySelector("form")
-// let email = document.querySelector("#email")
-// let name = document.querySelector("#name")
-// let age = document.querySelector("#age")
-// let photo = document.querySelector("#photo")
-// let errorAlert = document.querySelector(".error")
 
-// form.addEventListener("submit", async (event) => {
-//   event.preventDefault()
-//   errorAlert.innerText = ""
-//     try {
-//         let res = await axios.post("http://localhost:3000/users", {
-//           email: email.value,    
-//           name: name.value,
-//           username: username.value,
-//           age: age.value,
-//           photo_url: photo.value
-//           });
-//         window.location.href = "../HomePage/index.html"
-//         sessionStorage.setItem("currentUser", username.value)
-//     } catch (error) {
-//       errorAlert.innerText = "Please Enter a Valid Username and Email"
-//     }
-//   form.reset()
-// })
+
 
 
 // document.addEventListener("DOMContentLoaded",()=>{
